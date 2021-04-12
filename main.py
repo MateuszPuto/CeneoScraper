afd8311310de311689ba3111f6dd84970f6d4794
MateuszPuto <mateuszmputo@gmail.com>
2021-04-12 18:16:27 +0200

    2021-04-12 mp

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-respons = requests.get("https://www.ceneo.pl/22496575;02514;#tag=pp4")
+opinionList = []
+respons = requests.get("https://www.ceneo.pl/22496575")
+page = 0
 
-pageDOM = BeautifulSoup(respons.text, 'html.parser')
-opinion = pageDOM.select("div.js_product-review").pop(0)
+while respons.status_code == 200:
+    page += 1
+    respons = requests.get("https://www.ceneo.pl/22496575/opinie-"+str(page), allow_redirects=False)
 
-opinionId = opinion["data-entry-id"]
-author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
-try:
-    rcmd = opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
-except IndexError:
-    rcmd = None
-stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
-content = opinion.select("div.user-post__text").pop(0).get_text().strip()
-pros = opinion.select("div[class*=\"positives\"] ~ div.review-feature__item")
-pros = [item.get_text().strip() for item in pros]
-cons = opinion.select("div[class*=\"negatives\"] ~ div.review-feature__item")
-cons = [item.get_text().strip() for item in cons]
-try:
-    purchased = opinion.select("div.review-pz").pop(0).get_text().strip()
-except IndexError:
-    purchased = None
-publishDate = opinion.select("span.user-post__published > time:first-child").pop(0)["datetime"].strip()
-try:
-    purchaseDate = opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
-except IndexError:
-    purchaseDate = None
-useful = opinion.select("span[id^=\"votes-yes\"]").pop(0).get_text().strip()
-useless = opinion.select("span[id^=\"votes-no\"]").pop(0).get_text().strip()
+    pageDOM = BeautifulSoup(respons.text, 'html.parser')
+    opinions = pageDOM.select("div.js_product-review")
 
-print(content)
+    for opinion in opinions:
+        opinionId = opinion["data-entry-id"]
+        author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
+        try:
+            rcmd = opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
+            rcmd = True if rcmd == "Polecam" else False
+        except IndexError:
+            rcmd = None
+        stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
+        stars = float(stars.split('/')[0].replace(',', '.'))
+        content = opinion.select("div.user-post__text").pop(0).get_text().strip()
+        content.replace("\n", " ").replace("\r", " ")
+        pros = opinion.select("div[class*=\"positives\"] ~ div.review-feature__item")
+        pros = [item.get_text().strip() for item in pros]
+        cons = opinion.select("div[class*=\"negatives\"] ~ div.review-feature__item")
+        cons = [item.get_text().strip() for item in cons]
+        try:
+            purchased = bool(opinion.select("div.review-pz").pop(0).get_text().strip())
+        except IndexError:
+            purchased = False
+        publishDate = opinion.select("span.user-post__published > time:first-child").pop(0)["datetime"].strip()
+        try:
+            purchaseDate = opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
+        except IndexError:
+            purchaseDate = None
+        useful = int(opinion.select("span[id^=\"votes-yes\"]").pop(0).get_text().strip())
+        useless = int(opinion.select("span[id^=\"votes-no\"]").pop(0).get_text().strip())
+
+        opinionDict = {
+            "opinionId": opinionId,
+            "author": author,
+            "rcmd": rcmd,
+            "stars": stars,
+            "content": content,
+            "pros": pros,
+            "cons": cons,
+            "purchased": purchased,
+            "publishDate": publishDate,
+            "purchaseDate": purchaseDate,
+            "useful": useful,
+            "useless": useless
+        }
+        opinionList.append(opinionDict)
+
+with open("./opinions/22496575.json", "w", encoding="UTF-8") as f:
+    json.dump(opinionList[:-1], f, indent=4, ensure_ascii=False)
